d31ee9a714205f0b84092b84c09edf90d6f52344
Alex Girao <fco.alex.girao@gmail.com>
2022-08-21 21:29:17 -0300

    endpoint e requeriments atualizado

---
+++ endpoint.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from joblib import load as load_joblib
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        frase = request.json["frase"]
+        pipeline = load_joblib("models/sentiments.joblib")
+        vector = pipeline.named_steps.vectorizer.transform([frase])
+        response = pipeline.named_steps.model.predict(vector)
+        return {"response": response[0]}, 200
+    except Exception as e:
+        print(e)
+        return {"error": "Falha ao tentar realizar a predição."}, 500
